9e85b60794ac7ccc9abceee2d1fe22c3fea07591
vajrojuvinay412-commits <vajrojuvinay412@gmail.com>
2025-11-22 00:10:32 +0530

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "VBEATS",
+  description: "Gen-Z Advanced Photo | Video | Audio Editing Platform",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
